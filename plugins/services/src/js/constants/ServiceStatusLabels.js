69f8fe2d8845c0a572587a886cc50e2056120b47
杨洋 <yangyang63@jd.com>
2017-06-28 14:50:44 +0800

    部分遗漏的汉化

--- plugins/services/src/js/constants/ServiceStatusLabels.js
+++ plugins/services/src/js/constants/ServiceStatusLabels.js
@@ -1,10 +1,10 @@
 var ServiceStatusLabels = {
-  RUNNING: "正在运行",
-  DEPLOYING: "正在部署",
-  SUSPENDED: "已暂停",
+  RUNNING: "RUNNING",
+  DEPLOYING: "DEPLOYING",
+  SUSPENDED: "SUSPENDED",
   NA: "N/A",
-  DELAYED: "已推迟",
-  WAITING: "正在等待"
+  DELAYED: "DELAYED",
+  WAITING: "WAITING"
 };
 
 module.exports = ServiceStatusLabels;
