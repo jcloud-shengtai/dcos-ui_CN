9370d22e0e73e77b64fa3cb6b85927487ed8d90f
hanchao5 <hanchao5@jd.com>
2017-06-30 17:10:13 +0800

    更新加载图

--- src/js/components/icons/IconDCOSLogoMark.js
+++ src/js/components/icons/IconDCOSLogoMark.js
@@ -5,8 +5,8 @@ class IconDCOSLogoMark extends React.Component {
     return (
       <svg
         className={this.props.className}
-        width="60"
-        height="75"
+        width="200"
+        height="200"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         {...this.props}
